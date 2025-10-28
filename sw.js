3ff2b973b2cd6aa3b8f739136570057f3c68354e
samuelecerga-afk <samuelecerga@gmail.com>
2025-10-28 16:12:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'kpi-wrapper-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === self.location.origin) {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  }
+});
